e00fc18dc807accfecca21002c1983c36c5e9a5d
Steve <samrithketisak.hun@gmail.com>
2023-07-31 11:27:22 +1000

    Add new Python file for GUI calculator

---
+++ gui_calculator.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.ttk import Radiobutton, Style
+
+# Back end
+
+# Front end
+# Set up calculator window
+calc_window = Tk()
+calc_window.title("Smart Calculator")
+
+
+# Lock window resize
+calc_window.resizable(0, 0)
+
+# Start the event loop
+calc_window.mainloop()
