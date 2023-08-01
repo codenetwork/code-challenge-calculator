8043fd7d929d503675a26285a7d3d0a6bb94dc3a
Corey McCormick <coreymccor@live.com.au>
2023-08-01 16:57:40 +1000

    Added notches and graph label

--- 2-graph-calculator/graph.py
+++ 2-graph-calculator/graph.py
@@ -10,15 +10,35 @@ goto(0, 250)
 pd()
 goto(0,-250)
 
+# draw the notches
+for x in range(-240, 240, 20):
+    pu()
+    goto(x, 5)
+    pd()
+    goto(x, -5)
+
+for y in range(-240, 240, 20):
+    pu()
+    goto(-5, y)
+    pd()
+    goto(5, y)
+
+# enter some values
 startx = int(input("Enter your start x value: "))
 endx = int(input("Enter your end x value: "))
 equation = input("Enter your equation (x is the variable): ")
 
+# begin graphing
 pu()
 goto(startx*20, eval(equation, {"x": startx})*20)
 pd()
 for x in range(startx, endx):
     print(x, eval(equation))
     goto(x*20, eval(equation)*20)
+
+# label the graph
+pu()
+goto(200, 200)
+write(equation)
 
 done()
